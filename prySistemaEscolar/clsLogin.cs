06461bf1b8e50a1280d7ca61375332e09801955c
leonel <leonel@HERNANDEZ>
2026-06-11 10:29:55 -0600

    realice la clase login

---
+++ prySistemaEscolar/clsLogin.cs
@@ -0,0 +1,61 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySistemaEscolar
+{
+    internal class clsLogin
+    {
+        private string usuario;
+        private string password;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Password { get => password; set => password = value; }
+
+        public static string perfil;
+
+        public bool ValidarAcceso()
+        {
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT perfil FROM tblUsuarios " +
+                                 "WHERE nombreUsuario = @usuario AND password = @password;";
+
+                    using (var consulta = new MySqlCommand(sql, conexion))
+                    {
+                        consulta.Parameters.AddWithValue("@usuario", usuario);
+                        consulta.Parameters.AddWithValue("@password", password);
+
+                        using (var resultado = consulta.ExecuteReader())
+                        {
+                            if (resultado.Read())
+                            {
+                                perfil = resultado.GetString("perfil");
+                                MessageBox.Show("Tu perfil es: " + perfil, "Sistema");
+                                return true;
+                            }
+                            else
+                            {
+                                throw new Exception("Usuario o contraseña incorrectos.");
+                            }
+                        }// Liberar MySqlDateReader
+                    }// Liberar MySqlCommand
+                }// liberar MySqlConnection a través de tu clase 
+
+            }
+            catch (Exception ex)
+            {
+                //Si ocurre un error, lo relanzamos para que lo capture el formulario
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+
+    }
+}
