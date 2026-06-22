b1c987e4a47c53f4ee1b3beb00161dccde775d52
laryb <laryb@LARY>
2026-06-22 07:35:02 -0600

    Hice el codigo de la clase Carreras

--- prySistemaEscolar/clsCarreras.cs
+++ prySistemaEscolar/clsCarreras.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,22 @@ namespace prySistemaEscolar
 {
     internal class clsCarreras
     {
+        //Usamos un adaptador
+        private MySqlDataAdapter consulta;
+        //Usamos una tabla temporal
+        private DataTable tabla;
 
+        //Metodo para cargar datos en el DataGrid
+        public DataTable CargarDataGrid()
+        {
+            tabla = new DataTable();
+
+            clsConexion conexionBD = new clsConexion();
+            var conexion = conexionBD.AbrirConexion();
+            string sql = "select idCarrera AS Clave,nombreCarrera AS Carrera,descripcion AS Descripción from tblCarreras;";
+            consulta = new MySqlDataAdapter(sql, conexion);
+            consulta.Fill(tabla);
+            return tabla;
+        }
     }
 }
