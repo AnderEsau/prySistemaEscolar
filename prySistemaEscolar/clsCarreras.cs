56a5a0f685a7cd1060c3dfab28d35f73157aed2f
Samantha Castellanos <samanthayoulet@outlook.com>
2026-06-23 13:12:03 -0600

    realice el metodo de guardar actualizar en la clase de carreras

--- prySistemaEscolar/clsCarreras.cs
+++ prySistemaEscolar/clsCarreras.cs
@@ -11,13 +11,20 @@ namespace prySistemaEscolar
     internal class clsCarreras
     {
         private string nombreCarrera;
-        //Usamos un adaptador
+        private string descripcion;
+        private int idCarrera;//Este atributo es para referencia en update y delete
+        //usamos un adaptador
         private MySqlDataAdapter consulta;
+        //Usamos un command para insertar o actualizar
+        private MySqlCommand comando;
         //Usamos una tabla temporal
-        private DataTable tabla;
+        private DataTable tabla;  
+
 
         //propiedad para el atributo nombreCarrera  
         public string NombreCarrera { get => nombreCarrera; set => nombreCarrera = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int IdCarrera { get => idCarrera; set => idCarrera = value; }
 
         //Metodo para cargar datos en el DataGrid
         public DataTable CargarDataGrid()
@@ -71,6 +78,63 @@ namespace prySistemaEscolar
                 throw new Exception("Error en la conexión de la base de datos " + ex.Message);
             }
             return tabla;
+        }
+        //metodo para consultar
+        public string GuardarActualizar(int tipoOperacion)
+        {
+            string msg = "";
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    switch (tipoOperacion)
+                    {
+                        case 0://Insertar new
+                            string sqlN = "INSERT INTO tblCarreras(nombreCarrera,descripcion) VALUES(@nombreCarrera,@descripcion);";
+                            using (comando = new MySqlCommand(sqlN, conexion))
+                            {
+                                comando.Parameters.AddWithValue("nombreCarrera", nombreCarrera);
+                                comando.Parameters.AddWithValue("descripcion", descripcion);
+
+                                int filasAfectadas = comando.ExecuteNonQuery();
+                                if (filasAfectadas > 0)
+                                {
+                                    msg = "EL registro se guardo correctamente";
+                                }
+                                else
+                                {
+                                    msg = "Error, No se guardaron los datos...";
+                                }
+                            }//Libera la operacion de inserción
+                            break;
+                        case 1://Actualizar old
+                            string sqlA = "UPDATE tblCarreras C SET C.nombreCarrera=@nombreCarrera,C.descripcion=@descripcion WHERE C.idCarrera=@idCarrera;";
+                            using (comando = new MySqlCommand(sqlA, conexion))
+                            {
+                                comando.Parameters.AddWithValue("idCarrera", idCarrera);
+                                comando.Parameters.AddWithValue("nombreCarrera", nombreCarrera);
+                                comando.Parameters.AddWithValue("descripcion", descripcion);
+
+                                int filasAfectadas = comando.ExecuteNonQuery();
+                                if (filasAfectadas > 0)
+                                {
+                                    msg = "EL registro se guardo correctamente";
+                                }
+                                else
+                                {
+                                    msg = "Error, No se guardaron los datos...";
+                                }
+                            }//Libera la operacion de actualización
+                            break;
+                    }
+                }//Libera la conexión
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+            }
+            return msg;
         }
     }
 }      
