bf53c7e6c226669f522cafeaf8f95d73515dda52
leonel <leonel@HERNANDEZ>
2026-06-22 19:29:40 -0600

    realize la clase carrera

--- prySistemaEscolar/clsCarreras.cs
+++ prySistemaEscolar/clsCarreras.cs
@@ -10,10 +10,14 @@ namespace prySistemaEscolar
 {
     internal class clsCarreras
     {
+        private string nombreCarrera;
         //Usamos un adaptador
         private MySqlDataAdapter consulta;
         //Usamos una tabla temporal
         private DataTable tabla;
+
+        //propiedad para el atributo nombreCarrera  
+        public string NombreCarrera { get => nombreCarrera; set => nombreCarrera = value; }
 
         //Metodo para cargar datos en el DataGrid
         public DataTable CargarDataGrid()
@@ -27,5 +31,32 @@ namespace prySistemaEscolar
             consulta.Fill(tabla);
             return tabla;
         }
+
+        //Metodo para consultar por coincidencia 
+        public DataTable Consultar()
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT idCarrera AS Clave,nombreCarrera AS Carrera,descripcion AS Descripción FROM tblCarreras WHERE nombreCarrera LIKE @carrera;";
+                    using (var consultar = new MySqlCommand(sql, conexion))
+                    {
+                        consultar.Parameters.AddWithValue("@carrera", "%" + nombreCarrera + "%");
+                        using (consulta = new MySqlDataAdapter(consultar))
+                        {
+                            consulta.Fill(tabla);
+                        }//Libera el adaptador
+                    }//Libera la consulta
+                }//Libera la conexión
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la conexión de la base de datos " + ex.Message);
+            }
+            return tabla;
+        }
     }
-}
+}      
