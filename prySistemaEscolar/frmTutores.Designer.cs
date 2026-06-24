92502df5ba28515196f0a30e60332dc3e6223e29
leonel <leonel@HERNANDEZ>
2026-06-23 20:40:57 -0600

    realize frmTutor

--- prySistemaEscolar/frmTutores.Designer.cs
+++ prySistemaEscolar/frmTutores.Designer.cs
@@ -28,18 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTutores));
+            lblTutores = new Label();
+            pnlAgrupaControles = new Panel();
+            txtCorreo = new TextBox();
+            txtTelefono = new TextBox();
+            txtDireccion = new TextBox();
+            txtParentesco = new TextBox();
+            txtNombreT = new TextBox();
+            btnNuevo = new Button();
+            txtGuardar = new Button();
+            txtEliminar = new Button();
+            dgvTutores = new DataGridView();
+            txtNombreTutor = new TextBox();
+            pnlAgrupaControles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvTutores).BeginInit();
             SuspendLayout();
+            // 
+            // lblTutores
+            // 
+            lblTutores.AutoSize = true;
+            lblTutores.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTutores.Location = new Point(280, 33);
+            lblTutores.Name = "lblTutores";
+            lblTutores.Size = new Size(196, 65);
+            lblTutores.TabIndex = 3;
+            lblTutores.Text = "Tutores";
+            // 
+            // pnlAgrupaControles
+            // 
+            pnlAgrupaControles.Controls.Add(txtCorreo);
+            pnlAgrupaControles.Controls.Add(txtTelefono);
+            pnlAgrupaControles.Controls.Add(txtDireccion);
+            pnlAgrupaControles.Controls.Add(txtParentesco);
+            pnlAgrupaControles.Controls.Add(txtNombreT);
+            pnlAgrupaControles.Controls.Add(btnNuevo);
+            pnlAgrupaControles.Controls.Add(txtGuardar);
+            pnlAgrupaControles.Controls.Add(txtEliminar);
+            pnlAgrupaControles.Location = new Point(59, 110);
+            pnlAgrupaControles.Name = "pnlAgrupaControles";
+            pnlAgrupaControles.Size = new Size(417, 288);
+            pnlAgrupaControles.TabIndex = 4;
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtCorreo.Location = new Point(3, 167);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.PlaceholderText = "Escribe el correo";
+            txtCorreo.Size = new Size(408, 35);
+            txtCorreo.TabIndex = 10;
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtTelefono.Location = new Point(3, 126);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.PlaceholderText = "Escribe el telefono";
+            txtTelefono.Size = new Size(408, 35);
+            txtTelefono.TabIndex = 9;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtDireccion.Location = new Point(3, 85);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.PlaceholderText = "Escribe la dirección";
+            txtDireccion.Size = new Size(408, 35);
+            txtDireccion.TabIndex = 8;
+            // 
+            // txtParentesco
+            // 
+            txtParentesco.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtParentesco.Location = new Point(3, 44);
+            txtParentesco.Name = "txtParentesco";
+            txtParentesco.PlaceholderText = "Escribe el parentesco";
+            txtParentesco.Size = new Size(408, 35);
+            txtParentesco.TabIndex = 7;
+            // 
+            // txtNombreT
+            // 
+            txtNombreT.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtNombreT.Location = new Point(3, 3);
+            txtNombreT.Name = "txtNombreT";
+            txtNombreT.PlaceholderText = "Escribe el nombre del tutor";
+            txtNombreT.Size = new Size(408, 35);
+            txtNombreT.TabIndex = 6;
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.BackgroundImage = (Image)resources.GetObject("btnNuevo.BackgroundImage");
+            btnNuevo.BackgroundImageLayout = ImageLayout.Stretch;
+            btnNuevo.Location = new Point(3, 219);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(96, 46);
+            btnNuevo.TabIndex = 5;
+            btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // txtGuardar
+            // 
+            txtGuardar.BackgroundImage = (Image)resources.GetObject("txtGuardar.BackgroundImage");
+            txtGuardar.BackgroundImageLayout = ImageLayout.Stretch;
+            txtGuardar.Location = new Point(156, 219);
+            txtGuardar.Name = "txtGuardar";
+            txtGuardar.Size = new Size(96, 46);
+            txtGuardar.TabIndex = 4;
+            txtGuardar.UseVisualStyleBackColor = true;
+            txtGuardar.Click += button2_Click_1;
+            // 
+            // txtEliminar
+            // 
+            txtEliminar.BackgroundImage = (Image)resources.GetObject("txtEliminar.BackgroundImage");
+            txtEliminar.BackgroundImageLayout = ImageLayout.Stretch;
+            txtEliminar.Location = new Point(315, 219);
+            txtEliminar.Name = "txtEliminar";
+            txtEliminar.Size = new Size(96, 46);
+            txtEliminar.TabIndex = 3;
+            txtEliminar.UseVisualStyleBackColor = true;
+            // 
+            // dgvTutores
+            // 
+            dgvTutores.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTutores.Location = new Point(491, 154);
+            dgvTutores.Name = "dgvTutores";
+            dgvTutores.Size = new Size(392, 244);
+            dgvTutores.TabIndex = 11;
+            // 
+            // txtNombreTutor
+            // 
+            txtNombreTutor.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
+            txtNombreTutor.Location = new Point(616, 113);
+            txtNombreTutor.Name = "txtNombreTutor";
+            txtNombreTutor.PlaceholderText = "Buscar Tutor";
+            txtNombreTutor.Size = new Size(164, 35);
+            txtNombreTutor.TabIndex = 12;
             // 
             // frmTutores
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1343, 853);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(940, 449);
+            Controls.Add(txtNombreTutor);
+            Controls.Add(dgvTutores);
+            Controls.Add(pnlAgrupaControles);
+            Controls.Add(lblTutores);
+            Margin = new Padding(2);
             Name = "frmTutores";
-            Text = "frmTutores";
+            Text = "Registro de Tutores";
+            pnlAgrupaControles.ResumeLayout(false);
+            pnlAgrupaControles.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvTutores).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label lblTutores;
+        private Panel pnlAgrupaControles;
+        private TextBox txtNombreT;
+        private Button btnNuevo;
+        private Button txtGuardar;
+        private Button txtEliminar;
+        private TextBox txtCorreo;
+        private TextBox txtTelefono;
+        private TextBox txtDireccion;
+        private TextBox txtParentesco;
+        private DataGridView dgvTutores;
+        private TextBox txtNombreTutor;
     }
 }
--- prySistemaEscolar/frmTutores.cs
+++ prySistemaEscolar/frmTutores.cs
@@ -16,5 +16,17 @@ namespace prySistemaEscolar
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
