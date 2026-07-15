25d359c5fb153ebbd053dd8ad67642d66987b928
Samantha Castellanos <samanthayoulet@outlook.com>
2026-07-14 20:19:57 -0600

    CORREGI ERRORES

--- prySistemaEscolar/clsReporte.cs
+++ prySistemaEscolar/clsReporte.cs
@@ -20,7 +20,7 @@ namespace prySistemaEscolar
                 clsConexion conexionBD = new clsConexion();
                 using(var conexion = conexionBD.AbrirConexion())
                 {
-                    string sql = "SELEC A.matricula AS Matricula, " +
+                    string sql = "SELECT A.matricula AS Matricula, " +
                                 "A.nombreAlumno AS Nombre, " +
                                 "A.apellidoP AS 'A. Paterno, " +
                                 "A.apellidoM AS 'A. Materno, " +
@@ -52,7 +52,7 @@ namespace prySistemaEscolar
                 clsConexion conexionBD = new clsConexion();
                 using (var conexion = conexionBD.AbrirConexion())
                 {
-                    string sql = "SELEC A.matricula AS Matricula, " +
+                    string sql = "SELECT A.matricula AS Matricula, " +
                                 "A.nombreAlumno AS Nombre, " +
                                 "A.apellidoP AS 'A. Paterno, " +
                                 "A.apellidoM AS 'A. Materno, " +
