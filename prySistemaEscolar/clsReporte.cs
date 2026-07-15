53735850f2702af809a23b8ee714187780b188c0
Samantha Castellanos <samanthayoulet@outlook.com>
2026-07-14 18:23:35 -0600

    cree el codigo de frmreportes

---
+++ prySistemaEscolar/clsReporte.cs
@@ -0,0 +1,79 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySistemaEscolar
+{
+    internal class clsReporte
+    {
+        DataTable tabla;
+        MySqlDataAdapter consulta;
+        public DataTable ConsultarAlumnosRiesgo() 
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using(var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELEC A.matricula AS Matricula, " +
+                                "A.nombreAlumno AS Nombre, " +
+                                "A.apellidoP AS 'A. Paterno, " +
+                                "A.apellidoM AS 'A. Materno, " +
+                                "C.nombreCarrera AS Carrera, " +
+                                "A.promedioBachillerato AS Promedio " +
+                                "FROM tblAlumnos A " +
+                                "INNER JOIN tblcarreras C ON A.idCarrera = C.idCarrera " +
+                                "WHERE A.promedioBachillerato <= 8.0;";
+
+                   using(consulta=new MySqlDataAdapter (sql, conexion))//CONDICION
+                   {
+                        consulta.Fill(tabla);
+                   }//se libera la consulta
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar alumnos en riesgo:" + ex.Message);
+            }
+            return tabla;
+        }
+
+        public DataTable consultarAlumnosTutores()
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELEC A.matricula AS Matricula, " +
+                                "A.nombreAlumno AS Nombre, " +
+                                "A.apellidoP AS 'A. Paterno, " +
+                                "A.apellidoM AS 'A. Materno, " +
+                                "A.promedioBachillerato AS Promedio " +
+                                "T.nombreTutor"+
+                                "FROM tblAlumnos A"+
+                                "INNER JOIN tbltutores T ON A.idTutor = T.idTutor"+
+                                "WHERE A.promedioBachillerato >= 9.0;";
+
+                    using (consulta = new MySqlDataAdapter(sql, conexion))//CONDICION
+                    {
+                        consulta.Fill(tabla);
+                    }//se libera la consulta
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar alumnos y tutores:" + ex.Message);
+            }
+            return tabla;
+        }
+    }
+}
---
+++ prySistemaEscolar/reporte.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySistemaEscolar
+{
+    public partial class reporte : Form
+    {
+        clsReporte reportes;//objeto
+        public reporte()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            reportes = new clsReporte();
+
+             if(rdbOpcion1.Checked==true)
+             {
+                //llamar la consulta para cargar en el grid 
+                CargarGridAlumnosRiesgo();
+             }
+             else if(rdbOpcion2.Checked==true)
+             {
+                //llamar la consulta para cargar en el grid 
+                CargarGridAlumnosTutores();
+             }
+        }
+        public void CargarGridAlumnosRiesgo()
+        {
+            reportes = new clsReporte();
+            dgvReportes.DataSource = null;
+            dgvReportes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                dgvReportes.DataSource = reportes.ConsultarAlumnosRiesgo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void CargarGridAlumnosTutores()
+        {
+            reportes = new clsReporte();
+            dgvReportes.DataSource = null;
+            dgvReportes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                dgvReportes.DataSource = reportes.consultarAlumnosTutores();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
